9ff45989fdc9dafe9044dad4a763a1123de80134
matt <matt@renaud.us>
2018-01-23 19:53:48 -0500

    docId must be alphanumeric

--- src/docId.js
+++ src/docId.js
@@ -1,21 +1,11 @@
+import rand from "./rand";
 
-import rand from './rand';
+const { location: { hash: origHash } } = window;
 
-const {
-  location: {
-    hash: origHash
-  }
-} = window;
-
-if (!origHash || origHash.length < 3) {
+if (!origHash || origHash.length < 3 || !/^[a-z0-9]+$/i.test(origHash)) {
   window.location.hash = `#${rand()}`;
 }
 
-const {
-  location: {
-    hash: newHash
-  }
-} = window;
+const { location: { hash: newHash } } = window;
 
-
-export default newHash.slice(1)
+export default newHash.slice(1);
