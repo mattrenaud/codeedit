94b199dc6fb2e042ced9d465af03a0986e71c13e
matt <matt@renaud.us>
2018-01-23 20:06:53 -0500

    add 404

--- src/Nav.js
+++ src/Nav.js
@@ -3,8 +3,8 @@ import "./Nav.css";
 
 const Nav = () => (
   <nav className="Nav bg-light text-dark d-flex align-items-center">
-    <div class="d-flex flex-column">
-      <img alt="logo" className="logo mb-1" src="/logo.png" />
+    <div className="d-flex flex-column">
+      <img className="logo mb-1" alt="logo" src="/logo.png" />
       <span className="catch-phrase text-muted text-center font-weight-bold">
         simple collaborative code editing
       </span>
