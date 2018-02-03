5c0b1a94e298c84ef52d2705ccb56ddf0e44fc3f
matt <matt@renaud.us>
2018-02-03 11:46:57 -0500

    default language

--- src/Language.state.js
+++ src/Language.state.js
@@ -1,18 +1,17 @@
-
-const SET_LANGUAGE_SELECTION = 'SET_LANGUAGE_SELECTION';
+const SET_LANGUAGE_SELECTION = "SET_LANGUAGE_SELECTION";
 
 export const setLanguageSelection = selection => {
   return {
     type: SET_LANGUAGE_SELECTION,
     selection
-  }
-}
+  };
+};
 
-export function languageSelection(state = 'javascript', action) {
+export function languageSelection(state = "htmlmixed", action) {
   switch (action.type) {
     case SET_LANGUAGE_SELECTION:
-      return action.selection
+      return action.selection;
     default:
-      return state
+      return state;
   }
 }
